d6b4824c99c94f51f58d468244a65163f363b136
Kelton Hale <keltonhale33@gmail.com>
2021-10-18 15:03:08 -0600

    began creating Chart to display purchases through months

---
+++ src/components/Chart/Chart.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Chart.css";
+import ChartBar from "./ChartBar";
+
+const Chart = (props) => {
+  return (
+    <div className="chart">
+      {props.dataPoints.map((datapoint) => (
+        <ChartBar
+          key={datapoint.label}
+          value={datapoint.value}
+          maxValue={null}
+          label={datapoint.label}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default Chart;
