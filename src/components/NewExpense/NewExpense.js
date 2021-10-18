39542e7746def873023e00d3c8b09b87c4ef6efd
Kelton Hale <keltonhale33@gmail.com>
2021-10-18 14:41:43 -0600

    added button to newexpense to open and close space

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import ExpenseForm from "./ExpenseForm";
 import "./NewExpense.css";
 
 // CREATES A NEW EXPENSE FROM EXPENSEFORM.JS
 const NewExpense = (props) => {
+  const [isEditing, setIsEditing] = useState(false);
+
   // Takes in the data entered from ExpenseForm.js and puts into expeseData with an ID
   const saveExpenseDataHandler = (enteredExpenseData) => {
     //
@@ -13,15 +15,30 @@ const NewExpense = (props) => {
       //
     };
     props.onAddExpense(expenseData);
+    setIsEditing(false);
+  };
+
+  const startEditingHandler = () => {
+    setIsEditing(true);
+  };
+
+  const stopEditing = () => {
+    setIsEditing(false);
   };
 
   return (
     <div className="new-expense">
-      <ExpenseForm
-        /* onSaveExpenseData calls on the saveExpenseDataHandler function in this file, which will take in the data entered in the Expense form and generate it into a different expenseData object with the inclusion of a random ID */ onSaveExpenseData={
-          saveExpenseDataHandler
-        }
-      />
+      {!isEditing && (
+        <button onClick={startEditingHandler}>Add New Expense</button>
+      )}
+      {isEditing && (
+        <ExpenseForm
+          /* onSaveExpenseData calls on the saveExpenseDataHandler function in this component, which will take in the data entered in the Expense form and generate it into a different expenseData object with the inclusion of a random ID */ onSaveExpenseData={
+            saveExpenseDataHandler
+          }
+          onCancel={stopEditing}
+        />
+      )}
     </div>
   );
 };
